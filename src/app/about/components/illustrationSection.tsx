5cb86527642b269ebd2a2f0b4a854b8539b3c967
vaaghu <vignesh021102@gmail.com>
2024-02-24 23:16:28 +0530

    illustrations

--- src/app/about/components/illustrationSection.tsx
+++ src/app/about/components/illustrationSection.tsx
@@ -4,6 +4,7 @@ import {
   MutableRefObject,
   createRef,
   useState,
+  useLayoutEffect,
 } from "react";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/dist/ScrollTrigger";
@@ -12,126 +13,133 @@ import IllustrationBlock1 from "../assets/IllustrationBlock1";
 import IllustrationBlock2 from "../assets/IllustrationBlock2";
 import IllustrationBlock3 from "../assets/IllustrationBlock3";
 import IllustrationBlock4 from "../assets/IllustrationBlock4";
+import { time } from "console";
 
 gsap.registerPlugin(ScrollTrigger);
 
 export const IllustrationSection = () => {
-  const illustrationSectionRef = useRef(null);
-  const illustrationBlock1 = useRef(null);
+  const sectionRef: MutableRefObject<any> = useRef();
+  const illustationSectionRef: MutableRefObject<any> = useRef(null);
+  const illustrationBlock1: MutableRefObject<any> = useRef(null);
   const illustrationBlock2 = useRef(null);
   const illustrationBlock3 = useRef(null);
   const illustrationBlock4 = useRef(null);
-  const block1 = useRef(null);
-  const block2 = useRef(null);
-  const block3 = useRef(null);
-  const block4 = useRef(null);
+  const block1: MutableRefObject<any> = useRef(null);
+  const block2: MutableRefObject<any> = useRef(null);
+  const block3: MutableRefObject<any> = useRef(null);
+  const block4: MutableRefObject<any> = useRef(null);
   const illustrationDelay = 4;
-  const illustationTimeline = (
-    illustation: MutableRefObject<any>
-  ): gsap.core.Timeline => {
-    console.log(
-      illustation.current.offsetLeft / illustation.current.offsetWidth,
-      illustation.current
-    );
-    return gsap
-      .timeline()
-      .to(illustation.current, {
-        ease: "sine",
-        x:
-          -illustation.current.offsetWidth *
-          Math.floor(
-            illustation.current.offsetLeft / illustation.current.offsetWidth
-          ),
-        duration: 7,
-      })
-      .to(illustation.current, {
-        ease: "sine",
-        x:
-          -illustation.current.offsetWidth *
-          (Math.floor(
-            illustation.current.offsetLeft / illustation.current.offsetWidth
-          ) +
-            1),
-        delay: illustrationDelay,
-        durationPer: 5,
-        opacity: 0.5,
-      });
+  const illustationTimeline = (count?: number): gsap.core.Timeline => {
+    let panels = gsap.utils.toArray(".panel");
+
+    return gsap.timeline().to(panels, {
+      xPercent: -100 * (panels.length - 1),
+      ease: "none",
+      duration: 10,
+
+      scrollTrigger: {
+        trigger: sectionRef.current,
+        pin: sectionRef.current,
+        start: "top top",
+        scrub: true,
+        snap: {
+          snapTo: 1 / (panels.length - 1),
+          duration: 1,
+          delay: 0,
+          ease: "expo",
+        },
+        // end: () => "+=" + sectionRef.current.offsetWidth,
+        markers: true,
+      },
+    });
   };
-  const BlockTimeline = (
-    block: MutableRefObject<null>,
-    IllustrationBlock: gsap.core.Timeline
-  ): gsap.core.Timeline => {
+  const textTimeline = (): gsap.core.Timeline => {
+    let panels = gsap.utils.toArray(".textPanel");
+
+    return gsap.timeline().to(panels, {
+      yPercent: -100 * (panels.length - 1),
+      ease: "none",
+      duration: 1,
+      scrollTrigger: {
+        trigger: sectionRef.current,
+        // pin: sectionRef.current,
+        start: "top top",
+        scrub: true,
+        snap: {
+          snapTo: 1 / (panels.length - 1),
+          duration: 1,
+          ease: "expo",
+        },
+        // end: () => "+=" + sectionRef.current.offsetWidth,
+        markers: true,
+      },
+    });
+  };
+  const BlockTimeline = (block: MutableRefObject<null>): gsap.core.Timeline => {
     return gsap
-      .timeline()
+      .timeline({
+        scrollTrigger: {
+          trigger: sectionRef.current,
+          scrub: true,
+        },
+      })
       .set(block.current, {
-        y: "100dvh",
+        y: "100vh",
         opacity: 0,
       })
       .from(block.current, {
         duration: 5,
-        y: "100dvh",
+        y: "100vh",
         opacity: 0,
       })
-      .add(IllustrationBlock, "<")
       .to(block.current, {
         delay: 3,
         duration: 5,
-        y: "-100dvh",
+        y: "-100vh",
         opacity: 0,
       });
   };
-  useEffect(() => {
-    const ctx = gsap.context(() => {
-      const t1 = gsap.timeline({
-        scrollTrigger: {
-          trigger: "#section",
-          pin: "#section",
-          markers: true,
-          scrub: true,
-          start: "top top",
-          // end: "bottom center",
-        },
-      });
-      t1.add(illustationTimeline(illustrationBlock1), illustrationDelay * 0);
-      t1.add(illustationTimeline(illustrationBlock2), illustrationDelay * 2.5);
-      t1.add(illustationTimeline(illustrationBlock3), illustrationDelay * 4.5);
-      t1.add(illustationTimeline(illustrationBlock4), illustrationDelay * 6.5);
-    });
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      const timeline = gsap.timeline();
+      timeline.add(illustationTimeline());
+      // timeline.add(illustationTimeline(3, BlockTimeline(block1)));
+      textTimeline();
+    }, sectionRef);
     return () => ctx.revert();
-  }, []);
+  });
   return (
-    <section
-      className={styles.coinSection}
-      ref={illustrationSectionRef}
-      id="section">
-      <div className={styles.sectionHalfIllustration}>
+    <section className={styles.coinSection} ref={sectionRef} id="section">
+      <div
+        className={styles.sectionHalfIllustration}
+        ref={illustationSectionRef}>
         <div
-          className={styles.IllustrationBlock}
+          className={styles.IllustrationBlock + " panel"}
           id="illustration1"
           ref={illustrationBlock1}>
-          <IllustrationBlock1 width="40dvw" height="40dvh" />
+          <IllustrationBlock1 width="350px" height="350px" />
         </div>
         <div
-          className={styles.IllustrationBlock}
+          className={styles.IllustrationBlock + " panel"}
           id="illustration2"
           ref={illustrationBlock2}>
-          <IllustrationBlock2 width="40dvw" height="40dvh" />
+          <IllustrationBlock2 width="350px" height="350px" />
         </div>
         <div
-          className={styles.IllustrationBlock}
+          className={styles.IllustrationBlock + " panel"}
           id="illustration3"
           ref={illustrationBlock3}>
-          <IllustrationBlock3 width="40dvw" height="40dvh" />
+          <IllustrationBlock3 width="350px" height="350px" />
         </div>
         <div
-          className={styles.IllustrationBlock}
+          className={styles.IllustrationBlock + " panel"}
           id="illustration4"
           ref={illustrationBlock4}>
-          <IllustrationBlock4 width="40dvw" height="40dvh" />
+          <IllustrationBlock4 width="350px" height="350px" />
         </div>
       </div>
       <div className={styles.sectionHalfText}>
-        <div className={styles.block} ref={block1} id="block1">
+        <div className={styles.block + " textPanel"} ref={block1} id="block1">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore
           deleniti reprehenderit ratione minus cupiditate, praesentium beatae
           numquam. Veritatis fugit tempore ab, autem sapiente hic. Explicabo
@@ -139,7 +147,7 @@ export const IllustrationSection = () => {
           Sint tempora aperiam maiores quas vel architecto laudantium quos neque
           cumque odio, ducimus qui quia.
         </div>
-        <div className={styles.block} ref={block2} id="block2">
+        <div className={styles.block + " textPanel"} ref={block2} id="block2">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia,
           maiores dolore, saepe fugiat enim quis non dicta deserunt blanditiis
           necessitatibus aut harum beatae eius velit natus? Delectus fugit
@@ -147,7 +155,7 @@ export const IllustrationSection = () => {
           veniam nisi accusantium dolor animi autem facilis ad esse,
           necessitatibus magni laboriosam earum?
         </div>
-        <div className={styles.block} ref={block3} id="block3">
+        <div className={styles.block + " textPanel"} ref={block3} id="block3">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam omnis
           sint perspiciatis veniam culpa consectetur saepe? Quaerat dolorem
           perspiciatis reprehenderit dolores dicta minima. Blanditiis aliquid
@@ -155,7 +163,7 @@ export const IllustrationSection = () => {
           explicabo ea voluptates dicta corporis dolorem quidem at nam tenetur
           fuga consequuntur expedita aut magni odit.
         </div>
-        <div className={styles.block} ref={block4} id="block4">
+        <div className={styles.block + " textPanel"} ref={block4} id="block4">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis beatae
           deserunt sit quo voluptate fugit dolorum aspernatur numquam eum
           voluptatibus repellat iure modi in ratione tenetur natus, ipsa magni
